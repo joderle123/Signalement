f1efa0cb389c719362a6189fa121cb0e3a85e767
joderle123 <joderle123@hotmail.com>
2026-03-23 13:30:28 +0100

    Delete js directory

--- js/questionnaire.js
+++
@@ -1,9 +0,0 @@
-/* ============================================
-   Questionnaire Data - not used in letter format
-   Kept as placeholder for future extensions
-   ============================================ */
-
-// No questionnaire data needed for the MiTe letter format
-function buildQuestionnaire(lang) {
-    // No-op: letter format doesn't use questionnaire grids
-}
